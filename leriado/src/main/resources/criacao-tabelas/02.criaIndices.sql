12de2dd1cb60eb191b91fd4342937b11ca801597
Isleimar Oliveira <isleimar@hotmail.com>
2019-03-31 12:36:00 -0300

    Restruturando os scripts

--- leriado/src/main/resources/criacao-tabelas/02.criaIndices.sql
+++ leriado/src/main/resources/criacao-tabelas/02.criaIndices.sql
@@ -7,15 +7,7 @@ CREATE INDEX indice_texto_id ON texto(id);
 
 CREATE INDEX indice_comentario_id ON comentario(textoid);
 
-CREATE INDEX indice_publicacao_id ON publicacao(textoid);
-
-CREATE INDEX indice_foto_id ON foto(publicacaoid);
-
-CREATE INDEX indice_noticia_id ON noticia(publicacaoid);
-
-CREATE INDEX indice_link_id ON link(publicacaoid);
-
-CREATE INDEX indice_visualiza_id ON visualiza(publicacaoid,usuarioid);
+CREATE INDEX indice_visualiza_id ON visualiza(textoid,usuarioid);
 
 CREATE INDEX indice_segue_id ON segue(segueid, seguidoid);
 
@@ -27,11 +19,9 @@ CREATE INDEX indice_admgrupo_id ON admgrupo(usuarioid,grupoid);
 
 CREATE INDEX indice_participagrupo_id ON participagrupo(usuarioid,grupoid);
 
-CREATE INDEX indice_compartilha_id ON compartilha(usuarioid,publicacaoid,grupoid);
+CREATE INDEX indice_compartilha_id ON compartilha(usuarioid,textoid,grupoid);
 
-CREATE INDEX indice_fotonoticia_id ON fotonoticia(noticiaid,fotoid);
-
-CREATE INDEX indice_fotoperfil_id ON fotoperfil(usuarioid,fotoid);
+CREATE INDEX indice_fotoperfil_id ON fotoperfil(usuarioid,textoid);
 
 
 
